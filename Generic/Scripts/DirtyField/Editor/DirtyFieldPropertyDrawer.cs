9b6cb738d7f7d04c80768499ff1d1f754acad0b6
Ben Sherratt <44046941+btsherratt@users.noreply.github.com>
2022-01-21 09:46:06 +0000

    Extra change check

--- Generic/Scripts/DirtyField/Editor/DirtyFieldPropertyDrawer.cs
+++ Generic/Scripts/DirtyField/Editor/DirtyFieldPropertyDrawer.cs
@@ -5,9 +5,14 @@ using UnityEditor;
 public class DirtyFieldPropertyDrawer : PropertyDrawer {
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
         SerializedProperty valueProperty = property.FindPropertyRelative("m_value");
+
         EditorGUI.BeginChangeCheck();
+
+        //cur.serializedObject.Update();
         EditorGUI.PropertyField(position, valueProperty, label);
-        if (EditorGUI.EndChangeCheck()) {
+        //cur.serializedObject.ApplyModifiedProperties();
+        
+        if (EditorGUI.EndChangeCheck() && property.serializedObject.hasModifiedProperties) {
             IDirtyFieldPropertyDrawerExtensions dirtyField = fieldInfo.GetValue(property.serializedObject.targetObject) as IDirtyFieldPropertyDrawerExtensions;
             fieldInfo.SetValue(property.serializedObject.targetObject, dirtyField.EditedField());
         }
