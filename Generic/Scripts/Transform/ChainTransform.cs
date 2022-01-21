699d7b4dbada2f54106f561b7f48cb24e0f4cbc6
Ben Sherratt <44046941+btsherratt@users.noreply.github.com>
2022-01-21 14:12:25 +0000

    Chain

---
+++ Generic/Scripts/Transform/ChainTransform.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class ChainTransform : MonoBehaviour {
+    [Header("The transform to chain to")]
+    public ChainTransform m_chainedTransform;
+
+    [Header("The point at which you chain to your chained transform")]
+    public Vector3 m_chainPoint;
+
+    [Header("The point at which a chained transform will chain to you")]
+    public Vector3 m_attachPoint;
+
+    ChainTransform m_nextChainedTransform;
+
+    private void OnEnable() {
+        if (m_chainedTransform != null) {
+            m_chainedTransform.m_nextChainedTransform = this;
+        }
+    }
+
+    void LateUpdate() {
+        if (m_chainedTransform == null) {
+            ChainTransform parentChainTransform = this;
+            ChainTransform currentChainTransform = m_nextChainedTransform;
+            while (currentChainTransform != null) {
+                Vector3 targetPosition = parentChainTransform.transform.TransformPoint(parentChainTransform.m_attachPoint);
+                Vector3 currentPosition = currentChainTransform.transform.TransformPoint(currentChainTransform.m_chainPoint);
+                Vector3 currentAnglePosition = currentChainTransform.transform.TransformPoint(currentChainTransform.m_attachPoint);
+
+                Vector3 targetPositionDelta = targetPosition - currentPosition;
+                Vector3 targetAngleDelta = targetPosition - currentAnglePosition;
+                if (targetPositionDelta.sqrMagnitude > 0) {
+                    targetAngleDelta.Normalize();
+                    Quaternion targetRotation = Quaternion.Euler(0, 0, Mathf.Atan2(targetAngleDelta.y, targetAngleDelta.x) * Mathf.Rad2Deg - 90.0f);
+
+                    currentChainTransform.transform.rotation = targetRotation;
+
+                    currentChainTransform.transform.position = targetPosition - currentChainTransform.transform.TransformVector(currentChainTransform.m_chainPoint);
+                }
+
+                parentChainTransform = currentChainTransform;
+                currentChainTransform = currentChainTransform.m_nextChainedTransform;
+            }
+        }
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawWireSphere(transform.TransformPoint(m_chainPoint), 0.1f);
+        Gizmos.DrawWireSphere(transform.TransformPoint(m_attachPoint), 0.1f);
+    }
+}
