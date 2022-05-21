5b93ba8b1476c583a7ccb9922d1d27434cfca9f5
Ben Sherratt <44046941+btsherratt@users.noreply.github.com>
2022-05-21 14:15:11 +0100

    Look At

--- Generic/Scripts/Animation/LookAtIK.cs
+++ Generic/Scripts/Animation/LookAtIK.cs
@@ -6,17 +6,23 @@ public class LookAtIK : MonoBehaviour {
     public Transform m_target;
 
     Animator m_animator;
+    float m_lookAtWeight;
 
     private void OnEnable() {
         m_animator = GetComponent<Animator>();
     }
 
     private void OnAnimatorIK(int layerIndex) {
+        float targetWeight = 0.0f;
         if (m_target != null) {
-            m_animator.SetLookAtPosition(m_target.position);
-            m_animator.SetLookAtWeight(1.0f);//, 0.0f, 1.0f, 1.0f, 0.5f);
-        } else {
-            m_animator.SetLookAtWeight(0.0f);
+            float dot = Vector3.Dot(transform.InverseTransformPoint(m_target.position).normalized, Vector3.forward);
+            if (dot > 0.0f) {
+                m_animator.SetLookAtPosition(m_target.position);
+                targetWeight = 1.0f;//, 0.0f, 1.0f, 1.0f, 0.5f);
+            }
         }
+
+        m_lookAtWeight = Mathf.MoveTowards(m_lookAtWeight, targetWeight, Time.deltaTime);
+        m_animator.SetLookAtWeight(m_lookAtWeight);
     }
 }
