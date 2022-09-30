b875a65c5c148f1d3305bd008379199f7d894330
Ben Sherratt <44046941+btsherratt@users.noreply.github.com>
2022-09-30 12:01:15 +0100

    Hack for stopping fade up

--- Generic/Scripts/Camera/FadeUpForce.cs
+++ Generic/Scripts/Camera/FadeUpForce.cs
@@ -4,6 +4,8 @@ public class FadeUpForce : MonoBehaviour, CameraController.IFadeSource {
     public float m_fadeUpSpeed = 1.0f;
     public bool m_fadeDown;
 
+    public bool m_ignore;
+
     void Start() {
         Camera.main.GetComponent<CameraController>().PushControlSource(this, true, 0);
     }
@@ -12,8 +14,13 @@ public class FadeUpForce : MonoBehaviour, CameraController.IFadeSource {
     }
 
     public float GetCameraFade(Camera camera, CameraFadeCurtain fadeCurtain) {
-        float delta = m_fadeUpSpeed * (m_fadeDown ? Time.deltaTime : -Time.deltaTime);
-        float fade = Mathf.Clamp01(fadeCurtain.m_fadeAmount + delta);
+        float fade = fadeCurtain.m_fadeAmount;
+        
+        if (m_ignore != true) {
+            float delta = m_fadeUpSpeed * (m_fadeDown ? Time.deltaTime : -Time.deltaTime);
+            fade = Mathf.Clamp01(fadeCurtain.m_fadeAmount + delta);
+        }
+
         return fade;
     }
 }
